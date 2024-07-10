4bd7c1591c493559e115353bf6b1a93bb2584576
fredo <sausse.sylvain88@gmail.com>
2024-07-10 18:32:27 +0200

    added paralax bg and tileset for the map

--- player/Player.cs
+++ player/Player.cs
@@ -1,13 +1,13 @@
 using Godot;
 using System;
-using System.Reflection.Metadata;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 500.0f;
-	public const float JumpVelocity = 550.0f;
+	public const float Speed = 200.0f;
+	public const float JumpVelocity = 250.0f;
 
 	public float direction;
+	public float default_zoom;
 	public Camera2D cam;
 	public AnimatedSprite2D sprite;
 	public bool is_jumping;
@@ -21,11 +21,14 @@ public partial class Player : CharacterBody2D
 	{
 		cam = GetNode<Camera2D>("PlayerCamera");
 		sprite = GetNode<AnimatedSprite2D>("PlayerSprite");
+		default_zoom = cam.Zoom.X;
 		is_jumping = false;
 	}
 
 	public override void _Process(double delta) {
-		if (Mathf.Abs(direction) > 0.01) {
+		if (!IsOnFloor()) {
+			this.sprite.Play("jump");
+		} else if (Mathf.Abs(direction) > 0.01) {
 			if (direction < 0) {
 				this.sprite.Play("walking");
 				this.sprite.FlipH = true;
@@ -35,7 +38,6 @@ public partial class Player : CharacterBody2D
 			}
 		} else {
 			this.sprite.Play("idle");
-			this.sprite.Stop();
 		}
 	}
 
@@ -43,9 +45,9 @@ public partial class Player : CharacterBody2D
 	{
 		Rect2 view = GetViewportRect();
 
-		float new_aspect_ratio = 0.5f*(view.Size.Y/720);
+		//float new_aspect_ratio = default_zoom*(view.Size.Y/720);
 
-		cam.Zoom = new Vector2(new_aspect_ratio,new_aspect_ratio);
+		//cam.Zoom = new Vector2(new_aspect_ratio,new_aspect_ratio);
 
 		Vector2 velocity = Velocity;
 
@@ -56,12 +58,9 @@ public partial class Player : CharacterBody2D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		this.direction = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-		if (direction != 0)
-		{
+		if (direction != 0) {
 			velocity.X = this.direction * Speed;
-		}
-		else
-		{
+		} else {
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
 
@@ -70,18 +69,22 @@ public partial class Player : CharacterBody2D
 
 		MoveAndSlide();
 
+		//GetNode<Label>("hud_position_label").Text = "X:"+Position.X.ToString()+" Y:"+Position.Y.ToString();
+
 		// ------------CAMERA HANDLING-----------------
 
 		Vector2 velocity_normalized = velocity.Normalized();
 
-		float x = velocity_normalized.X*300;
-		float y = Mathf.Min(0, -670-this.Position.Y);
+		float x = velocity_normalized.X*(250/default_zoom);
+		float y = Mathf.Min(0, -100-Position.Y);
 
 		
-		float cam_pos_x = Mathf.MoveToward(this.cam.Position.X, x, Mathf.Sqrt(Math.Abs(this.cam.Position.X-x))*(float) delta*20);
-		float cam_pos_y = Mathf.MoveToward(this.cam.Position.Y, y, Mathf.Sqrt(Math.Abs(this.cam.Position.Y-y))*(float) delta*10);
+		float cam_pos_x = Mathf.MoveToward(this.cam.Position.X, x, Mathf.Sqrt(Math.Abs(this.cam.Position.X-x))*(float) delta*5);
+		float cam_pos_y = Mathf.MoveToward(this.cam.Position.Y, y, Mathf.Sqrt(Math.Abs(this.cam.Position.Y-y))*(float) delta*5);
 		
-		this.cam.Position = new Vector2(cam_pos_x,y);
+		this.cam.Position = new Vector2(cam_pos_x,cam_pos_y);
+
+		// ----------------------------------------------
 	}
 
 	public float HandleJump(double delta) {
