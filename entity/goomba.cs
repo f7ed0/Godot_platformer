8b93cdb4970059664c17fed0af6dd8b11242e9d7
fredo <sausse.sylvain88@gmail.com>
2024-08-08 14:48:33 +0200

    oui'

--- entity/goomba.cs
+++ entity/goomba.cs
@@ -6,12 +6,17 @@ public partial class goomba : CharacterBody2D
 	public const float Speed = 1000.0f;
 	public const float JumpVelocity = -400.0f;
 	public float direction = 1;
+	public poutre wisk_left,wisk_right;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
-	{	
+	public override void _Ready() {
+		wisk_left = GetNode<poutre>("wisk_left");
+		wisk_right = GetNode<poutre>("wisk_right");
+	}
+
+	public override void _PhysicsProcess(double delta) {	
 		Vector2 velocity = Velocity;
 		if (!IsOnFloor()) {
 			velocity.Y += gravity*0.25f;
@@ -28,6 +33,10 @@ public partial class goomba : CharacterBody2D
 				GD.Print(collision_angle);
 				direction = -1*direction;
 			}	
+		} else if (direction > 0 && !wisk_right.colliding) {
+			direction = -1*direction;
+		} else if (direction < 0 && !wisk_left.colliding) {
+			direction = -1*direction;
 		}
 		//GD.Print(Position);
 	}
